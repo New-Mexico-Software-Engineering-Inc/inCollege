9d5eebc2b3e449d389a9dc24ff5133d929983eb2
DESKTOP-N9VDHAM\John <johnnykoch02@gmail.com>
2023-09-24 12:02:18 -0400

    Changes

--- main.py
+++ main.py
@@ -4,53 +4,101 @@ import os
 import bcrypt
 from password_strength import PasswordPolicy
 
+class DatabaseManager:
+    def __init__(self, data_file):
+        self.conn = sqlite3.connect(data_file)
+        self.cursor = self.conn.cursor()
+
+    def execute(self, query, params=()):
+        try:
+            self.cursor.execute(query, params)
+            self.commit()
+        except sqlite3.Error as e:
+            print(f"Database error: {e}")
+            
+    def commit(self):
+        self.conn.commit()
+        
+    def fetch(self, query, params=()):
+        self.cursor.execute(query, params)
+        return self.cursor.fetchone()
+
+    def fetchall(self, query, params=()):
+        self.cursor.execute(query, params)
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.conn.close()
+    
+    def post_job(self, skill_name, long_description, job_title, job_description, employer, location, salary, user_id):
+        self.cursor.execute('''
+            INSERT INTO jobs (skill_name, long_description, job_title, job_description, employer, location, salary, posted_by)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?);''',
+            (skill_name, long_description, job_title, job_description, employer, location, salary, user_id))
+        self.commit()
+        return True
 
 
 class inCollegeAppManager:
-    """
-        Manages the inCollege App's database, login, and account creation.
-    """
-    
     def __init__(self, data_file="users.db", skills_file='data\example_skills.txt'):
-        """Initializes database connection and sets up password policy."""
-        # Establish database connection and create table if not exists
-        conn = sqlite3.connect(data_file)
-        cursor = conn.cursor()
-        cursor.execute('''
+        self.db_manager = DatabaseManager(data_file)
+        self.setup_database()
+        self._PasswordPolicy = PasswordPolicy.from_names(
+            length=8, uppercase=1, numbers=1, special=1,
+        )
+        
+
+    def setup_database(self):
+        self.db_manager.execute('''
         CREATE TABLE IF NOT EXISTS accounts (
-            username TEXT PRIMARY KEY,
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
             password TEXT NOT NULL,
             first_name TEXT NOT NULL,
             last_name TEXT NOT NULL
         );
         ''')
-        cursor.execute('''
+
+        self.db_manager.execute('''
         CREATE TABLE IF NOT EXISTS skills (
             skill_name TEXT PRIMARY KEY,
             long_description TEXT UNIQUE NOT NULL
         );
         ''')
-        conn.commit()
 
+        self.db_manager.execute('''
+        CREATE TABLE IF NOT EXISTS jobs (
+            job_id INTEGER PRIMARY KEY AUTOINCREMENT,  
+            skill_name TEXT NOT NULL,
+            long_description TEXT UNIQUE NOT NULL,
+            job_title TEXT NOT NULL,
+            job_description TEXT NOT NULL,
+            employer TEXT NOT NULL,
+            location TEXT NOT NULL,
+            salary REAL NOT NULL,
+            posted_by INTEGER,
+            FOREIGN KEY (posted_by) REFERENCES accounts(user_id)
+        );
+        ''')
+        self.db_manager.commit()
 
-        cursor.execute("SELECT COUNT(*) FROM skills")
-        # if table is empty, populate table from file
-        if cursor.fetchone()[0] == 0:
-            try:
-                with open(os.path.join(os.getcwd(), skills_file), 'r') as f:
-                    for line in f:
-                        skill_name, long_description = line.strip().split('$$$')
-                        cursor.execute("INSERT INTO skills (skill_name, long_description) VALUES (?, ?)", (skill_name, long_description))
-            except Exception as e:
-                print('Error while parsing skills file. Did you configre the file properly?')
-                print(e)
-            # Commit the changes
-            conn.commit()
-        self._db = conn
-        self._cursor = self._db.cursor()
-        self._PasswordPolicy = PasswordPolicy.from_names(
-            length = 8, uppercase = 1, numbers = 1, special = 1,
-        )
+        if self.db_manager.fetch("SELECT COUNT(*) FROM skills")[0] == 0:
+            self.populate_skills_from_file()
+
+    def populate_skills_from_file(self, skills_file='data\example_skills.txt'):
+        try:
+            with open(os.path.join(os.getcwd(), skills_file), 'r') as f:
+                for line in f:
+                    skill_name, long_description = line.strip().split('$$$')
+                    self.db_manager.execute("INSERT INTO skills (skill_name, long_description) VALUES (?, ?)", (skill_name, long_description))
+        except Exception as e:
+            print('Error while parsing skills file. Did you configure the file properly?')
+            print(e)
+
+    def _Terminate(self):
+        self.db_manager.close()
+        print('Goodbye!')
+        exit(0)
 
     def Run(self): # Can only Serve One Client at a time :(
         """Main loop for user interaction."""
@@ -63,10 +111,13 @@ class inCollegeAppManager:
             print("5. Play Demo Video\n")
         
         def additional_options(user):
+            """
+            Login options for a user
+            """
             def __LearnSkill():
                 # Fetch all records from the 'skills' table
-                self._cursor.execute("SELECT * FROM skills")
-                for i, row in enumerate(self._cursor.fetchall()):
+                self.db_manager.execute("SELECT * FROM skills")
+                for i, row in enumerate(self.db_manager.fetchall()):
                     skill_name, long_description = row
                     print(f"\nSkill {i+1}: {skill_name}, Description: {long_description}")
                 print('\nq: Quit')
@@ -81,8 +132,9 @@ class inCollegeAppManager:
                     verify = input("are you REALLY sure? (y/n)")
                     if(verify == "y"):
                         print("we are sorry to see you go")
-                        self._cursor.execute("DELETE FROM accounts WHERE username =?;",(user[0],))
+                        self.db_manager.execute("DELETE FROM accounts WHERE user_id =?;",(user[0],))
                         print(user)
+                        self.db_manager.commit()
                         return True
 
                 print("account creation cancled, returning to account menu")
@@ -91,27 +143,56 @@ class inCollegeAppManager:
             """
             TODO: Change this to work with main loop, implement "client/host connection" state transition logic.
             """
+            options = {'1':__SearchJob, '2': __ConnectWUser, '3': __LearnSkill, '4': _postJob, }
             while True:
-                print("\n1. Search for a job")
-                print("2. Find someone you know")
-                print("3. Learn a new skill")
-                print("4. Log out")
-                print("5. delete my account")
-                option = int(input("\nPlease Select an Option: "))
-                if option == 1:
-                    __SearchJob()
-                if option == 2:
-                    __ConnectWUser()
-                if option == 3:
-                    __LearnSkill()
-                if option == 4: break
-                if option == 5:
-                    verify = __DeleteThisAccount()
-                    if verify == True:
-                        break
+                print("\n1: Search for a job")
+                print("2: Find someone you know")
+                print("3: Learn a new skill")
+                print("4: Post a Job")
+                print("5: delete my account")
+                print("q: Log out")
 
+                option = input("\nPlease Select an Option: ")
+                if option == 'q': break
+                if option == '5': 
+                    if __DeleteThisAccount() == True: break
+                if option in options: options[option]()
+                
+        def _postJob():
+            """
+            Posts a job under the specified username
+            """
+            try:
+                # Capture job details
+                job_title = input("Enter the job title: \n")
+                job_description = input("Enter the job description: \n")
+                skill_name = input("Enter the required skill name: \n")
+                long_description = input("Enter a long description for the skill: \n")
+                employer = input("Enter the employer: \n")
+                location = input("Enter the location: \n")
+                salary = float(input("Enter the salary: \n"))  # Assuming salary is a floating-point number
 
+                assert job_title and job_description and skill_name and long_description and employer and location and salary, "Error: Cannot leave field Blank."
+
+                # Retrieve user's primary key
+                user = self.db_manager.execute('SELECT rowid FROM accounts WHERE user_id=?;', (user[0],)).fetchone()
+                if not user:
+                    raise Exception("User not found.")
+                user_id = user[0]
+
+                # Insert job details into jobs table
+                if not self.db_manager.post_job(skill_name, long_description, job_title, job_description, employer, location, salary, user_id):
+                    raise Exception("Could not create job.")
+                
+                print('Successfully Posted Job.')
+
+            except Exception as e:
+                print('Error While Posting Job:\n', e)
+                
         def _login_procedure():
+            """
+            UI Screen for logging in
+            """
             username = input("Enter your username: ")
             password = input("Enter your password: ")
             _acc = self.__login(username=username, password=password)
@@ -122,6 +203,9 @@ class inCollegeAppManager:
                 print('Incorrect username / password, please try again')
 
         def find_user_from_home_page():
+            """
+            Finds a user from the home page
+            """
             first_name = input("what is the first name of the person you are looking for:\n")
             last_name = input("what is the last name of the person you are looking for:\n")
             if self._is_person_in_database(first_name, last_name):
@@ -162,7 +246,7 @@ class inCollegeAppManager:
     
     def _Terminate(self):
         """Closes the database connection and exits the program."""
-        self._db.close()
+        self.db_manager.close()
         print('Goodbye!')    
         exit(0)
         
@@ -173,24 +257,33 @@ class inCollegeAppManager:
         def valid_password(password):
             """ Validates a Password based on requirements. """
             return not self._PasswordPolicy.test(password) and len(password) < 13
+        
         if not valid_password(password):
-                raise Exception("Invalid password. Please ensure it meets the requirements.")
-        if self._cursor.execute('SELECT COUNT(*) FROM accounts;').fetchone()[0] >= 5: raise Exception("All permitted accounts have been created. Please come back later.")
-        if self._cursor.execute('SELECT * FROM accounts WHERE username=?;', (username)).fetchone(): raise Exception("Username already exists. Please choose another one.")
+            raise Exception("Invalid password. Please ensure it meets the requirements.")
+
+        if self.db_manager.fetch('SELECT COUNT(*) FROM accounts;')[0] >= 5:
+            raise Exception("All permitted accounts have been created. Please come back later.")
+
+        if self.db_manager.fetch('SELECT * FROM accounts WHERE username=?;', (username,)):
+            raise Exception("Username already exists. Please choose another one.")
 
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        self._cursor.execute('INSERT INTO accounts (username, password, first_name, last_name) VALUES (?, ?, ?, ?);', (username, hashed_password, first_name, last_name))
-        self._db.commit()
+        self.db_manager.execute(
+            'INSERT INTO accounts (username, password, first_name, last_name) VALUES (?, ?, ?, ?);',
+            (username, hashed_password, first_name, last_name))
+        self.db_manager.commit()
+
         return self.__login(username=username, password=password)
+ # Returns the account from Database
 
     def __login(self, username, password):
-        user = self._cursor.execute('SELECT * FROM accounts WHERE username=?;', (username,)).fetchone()
+        user = self.db_manager.fetch('SELECT * FROM accounts WHERE username=?;', (username,))
         return user if user and bcrypt.checkpw(password.encode('utf-8'), user[1]) else None
 
+    # Updated to use db_manager
     def _is_person_in_database(self, first_name, last_name):
-        user = self._cursor.execute("SELECT * FROM accounts WHERE first_name=? AND last_name =?;", (first_name,last_name)).fetchone()
-        if user:
-            return True
+        user = self.db_manager.fetch("SELECT * FROM accounts WHERE first_name=? AND last_name =?;", (first_name, last_name))
+        return bool(user)
 
 # Introduction
 def main():
