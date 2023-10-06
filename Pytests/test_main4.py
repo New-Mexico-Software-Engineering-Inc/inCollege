be02aa0313e911a54e839e2d54415dd1da1a19a2
johnboiiii <145299227+johnboiiii@users.noreply.github.com>
2023-10-06 16:49:00 -0400

    created test 4 file

---
+++ Pytests/test_main4.py
@@ -0,0 +1,24 @@
+# Sprint 4 Test Cases
+# The functions use monkeypatch to mock input and capsys to capture output
+# Created by: Herbert J Keels,
+# Date created: 10/06/2023
+# Last Update: 
+
+import sqlite3
+import pytest
+import main
+from io import StringIO
+
+
+# function to run the inCollege program and return program output
+def runInCollege(capsys):
+    # Run the program, and collect the system exit code
+    with pytest.raises(SystemExit) as e:
+        main.InCollegeAppManager().Run()
+
+    # verify the exit code is 0
+    assert e.type == SystemExit
+    assert e.value.code == 0
+
+    # return the program's output
+    return capsys.readouterr()
